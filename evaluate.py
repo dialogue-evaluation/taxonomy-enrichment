ea72a27f3dbe4bf52fe49bf0631fea0f98c6d4e9
lilaspourpre <irina.nikishina@mail.ru>
2019-12-26 23:15:48 +0300

    update evaluation script: new computation of MAP

--- evaluate.py
+++ evaluate.py
@@ -1,52 +1,56 @@
 import argparse
 import codecs
+import json
 from collections import defaultdict
 
 
 # Consistent with Python 2
 
 
-def read_dataset(data_path, sep='\t'):
+def read_dataset(data_path, read_fn=lambda x: x, sep='\t'):
     vocab = defaultdict(list)
     with codecs.open(data_path, 'r', encoding='utf-8') as f:
         for line in f:
             line_split = line.replace("\n", '').split(sep)
             word = line_split[0]
-            hypernyms = line_split[1]
+            hypernyms = read_fn(line_split[1])
             vocab[word].append(hypernyms)
     return vocab
 
 
-def get_score(full, direct, predicted, k=10):
+def get_score(reference, predicted, k=10):
     ap_sum = 0
     rr_sum = 0
 
-    for neologism in full:
-        full_hypernyms = set(full.get(neologism, []))
-        direct_hypernyms = set(direct.get(neologism, []))
+    for neologism in reference:
+        reference_hypernyms = reference.get(neologism, [])
         predicted_hypernyms = predicted.get(neologism, [])
 
-        ap_sum += max(compute_ap(full_hypernyms, predicted_hypernyms, k),
-                      compute_ap(direct_hypernyms, predicted_hypernyms, k))
-        rr_sum += compute_rr(full_hypernyms, predicted_hypernyms, k)
-
-    return ap_sum / len(full), rr_sum / len(full)
+        ap_sum += compute_ap(reference_hypernyms, predicted_hypernyms, k)
+        rr_sum += compute_rr([j for i in reference_hypernyms for j in i], predicted_hypernyms, k)
+    return ap_sum / len(reference), rr_sum / len(reference)
 
 
 def compute_ap(actual, predicted, k=10):
     if not actual:
         return 0.0
 
-    if len(predicted) > k:
-        predicted = predicted[:k]
+    predicted = predicted[:k]
 
     score = 0.0
     num_hits = 0.0
-
+    already_predicted = set()
+    skipped = 0
     for i, p in enumerate(predicted):
-        if p in actual and p not in predicted[:i]:
-            num_hits += 1.0
-            score += num_hits / (i + 1.0)
+        if p in already_predicted:
+            skipped += 1
+            continue
+        for parents in actual:
+            if p in parents:
+                num_hits += 1.0
+                score += num_hits / (i + 1.0 - skipped)
+                already_predicted.update(parents)
+                break
 
     return score / min(len(actual), k)
 
@@ -60,17 +64,15 @@ def compute_rr(true, predicted, k=10):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('full')
-    parser.add_argument('direct')
+    parser.add_argument('reference')
     parser.add_argument('predicted')
     args = parser.parse_args()
 
-    full = read_dataset(args.full)
-    direct = read_dataset(args.direct)
+    reference = read_dataset(args.reference, lambda x: json.loads(x))
     submitted = read_dataset(args.predicted)
-    if set(full) != set(submitted):
-        print("Not all words are presented in your file")
-    mean_ap, mean_rr = get_score(full, direct, submitted)
+    if set(reference) != set(submitted):
+        print(f"Not all words are presented in your file: {len(reference)} vs {len(submitted)}")
+    mean_ap, mean_rr = get_score(reference, submitted, k=12)
     print("map: {0}\nmrr: {1}\n".format(mean_ap, mean_rr))
 
 
